5152f7b765daf0cc8d293da1fe37d7d6e926df13
ianoir <ian.studionoir@googlemail.com>
2021-11-02 14:46:21 +0000

    Updated boilerplate
    Updated supporting apps:
    Webpack v3 > v4
    Babel v6> @babel v7
    React-modal v2 > v3

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,47 @@
 const path = require('path');
+const MiniCssExtract = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, 
-        {
-            test: /\.s?css$/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+
+module.exports = (env) => {
+    const isProduction = env === 'production';
+
+    return {
+        entry: ['@babel/polyfill', './src/app.js'],
+        output: {
+            path: path.join(__dirname, 'public', 'dist'),
+            filename: 'bundle.js',
+            sourceMapFilename: '[file].map'
+        },
+        module: {
+            rules: [
+                {
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.s?css$/,
+                    use: [{
+                            loader: MiniCssExtract.loader
+                        },
+                        'css-loader',
+                        'sass-loader'
+                        ]
+                }
+            ]
+        },
+        plugins: [
+            new MiniCssExtract({
+                filename: "styles.css"
+            })
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true,
+            publicPath: '/dist/'
+        }
     }
 };
